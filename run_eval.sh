28739914e3a60851e6a1cac5334020717c863eae
Abid <abidaliawan@rocketmail.com>
2022-03-14 18:45:01 +0500

    eval edit

--- run_eval.sh
+++ run_eval.sh
@@ -1,1 +1,1 @@
-./eval.py --model_id ./Model --dataset ./Data --config ur --split test --chunk_length_s 5.0 --stride_length_s 1.0 --log_outputs
+python3 ./eval.py --model_id ./Model --dataset ./Data --config ur --split test --chunk_length_s 5.0 --stride_length_s 1.0 --log_outputs
